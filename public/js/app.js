ebc07ad1d4e70ecfcbdaaa4d3df2f6988c666382
PiotrBerebecki <petebere@gmail.com>
2017-01-09 13:35:46 +0700

    Update and draw an array of enemies

--- public/js/app.js
+++ public/js/app.js
@@ -6,60 +6,76 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
-// initial position
-let x = 10;
-let y = 10;
+// canvas dimensions
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
-// dimenstions
-let w = 20;
-let h = 20;
 
-// moving speed
-let speedX = 4;
-let speedY;
+// define enemies
+const enemies = [
+  {
+    x: 100,
+    y: 10,
+    w: 20,
+    h: 20,
+    speed: 2,
+  },
+  {
+    x: 200,
+    y: 10,
+    w: 20,
+    h: 20,
+    speed: 3,
+  },
+  {
+    x: 300,
+    y: 10,
+    w: 20,
+    h: 20,
+    speed: 4,
+  }
+];
+
+const enemyColor = 'tomato';
 
 
 const clearCanvas = () => {
-  ctx.clearRect(0, 0, 400, 300);
-};
-
-const draw = () => {
-  ctx.fillStyle = 'red';
-  ctx.fillRect(x, y, w, h);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 };
 
 
-
-const update = () => {
-  let isBeyondRightLimit = x >= 380;
-  let isBeyondLeftLimit = x <= 0;
-  
-  let isWithinSpeedYBoost = x >= 100 && x <= 150;
-  
-  if (isBeyondRightLimit) {
-    speedX = -speedX;
-  } else if (isBeyondLeftLimit) {
-    speedX = -speedX;
-  }
-  
-  if (isWithinSpeedYBoost) {
-    speedY = 1;
-    y = y + speedY;
-  } else {
-    speedY = 0;
-  }
-  
-  x = x + speedX;
+const drawEnemies = () => {
+  enemies.forEach(enemy => {
+    let { x, y, w, h } = enemy;
+    ctx.fillStyle = enemyColor;
+    ctx.fillRect(x - (w/2), y, w, h);
+  });
 };
 
 
+const updateEnemies = () => {
+  enemies.forEach(enemy => {
+    const { y, speed } = enemy;
+    
+    if (y >= 100) {
+      enemy.speed = -speed;
+    } else if (y <= 0) {
+      enemy.speed = -speed;
+    }
+    
+    enemy.y = y + enemy.speed;  
+  });
+};
+
+
+
+// start & pause logic
 let requestId;
 
 const start = () => {
   clearCanvas();
-  draw();
-  update();
-  
+  updateEnemies();
+  drawEnemies();
   requestId = window.requestAnimationFrame(start);
 };
 
